6a20920b1de58c3c06c19f9631fe5f0011ed1b2d
e <amha17@student.aau.dk>
2021-04-27 15:28:05 +0200

    make almost all default functions (currently in out.java)

--- P4_Compiler/ANTLR/src/main/java/out.java
+++ P4_Compiler/ANTLR/src/main/java/out.java
@@ -1,4 +1,151 @@
+import java.util.concurrent.TimeUnit;
+
 public class out{
+    //---default classes---
+    private class point{
+        private point(long _x, long _y){
+            x = _x;
+            y = _y;
+        }
+        long x;
+        long y;
+    }
+    private class line{
+        private line(point _A, point _B){
+            A = _A;
+            B = _B;
+        }
+        point A;
+        point B;
+    }
+    private class circle{
+        private circle(point _center, double _radius){
+            center = _center;
+            radius = _radius;
+        }
+        point center;
+        double radius;
+    }
+    private class triangle{
+        private triangle(point _A, point _B, point _C){
+            A = _A;
+            B = _B;
+            C = _C;
+        }
+        point A;
+        point B;
+        point C;
+    }
+    private class square{
+        private square(point _A, point _B, point _C, point _D){
+            A = _A;
+            B = _B;
+            C = _C;
+            D = _D;
+        }
+        point A;
+        point B;
+        point C;
+        point D;
+    }
+    private static class canvas{
+        //TBD
+
+        private static void draw(point toDraw){
+            //TBD
+        }
+        private static void draw(line toDraw){
+            //TBD
+        }
+        private static void draw(circle toDraw){
+            //TBD
+        }
+        private static void draw(triangle toDraw){
+            //TBD
+        }
+        private static void draw(square toDraw){
+            //TBD
+        }
+        private static void clear(){
+            //TBD
+        }
+    }
+    //---default functions---
+    //default angle functions
+    private static double arccos(double length){
+        return Math.acos(length);
+    }
+    private static double arcsin(double length){
+        return Math.asin(length);
+    }
+    private static double arctan(double length){
+        return Math.atan(length);
+    }
+    private static double cos(double angle){
+        return Math.cos(angle);
+    }
+    private static double sin(double angle){
+        return Math.sin(angle);
+    }
+    private static double tan(double angle){
+        return Math.tan(angle);
+    }
+    //other default functions
+    public static void clearcanvas(){
+        canvas.clear();
+    }
+    private static void draw(point toDraw){
+        canvas.draw(toDraw);
+    }
+    private static void draw(line toDraw){
+        canvas.draw(toDraw);
+    }
+    private static void draw(circle toDraw){
+        canvas.draw(toDraw);
+    }
+    private static void draw(triangle toDraw){
+        canvas.draw(toDraw);
+    }
+    private static void draw(square toDraw){
+        canvas.draw(toDraw);
+    }
+    //drawAll TBD
+    //move TBD
+    private static double root(double value, double rootOf){
+        return Math.pow(value, 1/rootOf);
+    }
+    private static double sqrt(double value){
+        return Math.sqrt(value);
+    }
+    private static void wait(double milliseconds) throws InterruptedException {
+        if(milliseconds < Math.pow(10, 2-3)){
+            long nanoseconds = (long)(milliseconds*1000*1000);
+            TimeUnit.NANOSECONDS.sleep(nanoseconds);
+        }
+        else if(milliseconds < Math.pow(10, 2)){
+            long microseconds = (long)(milliseconds*1000);
+            TimeUnit.MICROSECONDS.sleep(microseconds);
+        }
+        else if(milliseconds < Math.pow(10, 2+3)){
+            TimeUnit.MILLISECONDS.sleep((long)milliseconds);
+        }
+        else if(milliseconds < Math.pow(10, 2+6)){
+            long seconds = (long)(milliseconds/1000);
+            TimeUnit.SECONDS.sleep(seconds);
+        }
+        else if(milliseconds < Math.pow(10, 2+9)){
+            long hours = (long)(milliseconds/(1000*60*60));
+            TimeUnit.HOURS.sleep(hours);
+        }
+        else{
+            long days = (long)(milliseconds/(1000*60*60*24));
+            TimeUnit.DAYS.sleep(days);
+        }
+    }
+
+
+
+
     private static long a = (((-5) + (2 * ((long)Math.pow(6, 7)))) + ((6 - 4)));
     private static long asda;
     private static double foo(long asda) {
@@ -10,5 +157,7 @@ public class out{
         if((a == 4)) {
             a = 0;
         }
+        double time = 1;
+        wait(time);
     }
 }
